4cac7bcfd0eac1b1dba6475143bb891f9af49802
eliyev-1 <designman699@gmail.com>
2023-12-03 22:13:57 +0400

    save

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,67 @@ searchInput.classList.toggle('active')
     siteLeftHeaderList.classList.toggle('active')
     
  }
+ function checkHandler(checkboxId) {
+   let checkboxes = document.querySelectorAll('.site__right__license input[type="checkbox"]');
+   
+   checkboxes.forEach(function(checkbox) {
+       if (checkbox.id !== checkboxId) {
+           checkbox.checked = false;
+       }
+   });
+}
+let iconDetailBackground=document.querySelector('.icon__detail__background')
+let iconBoxList = document.querySelectorAll('.icon__box')
+
+iconBoxList.forEach((iconBox) => {
+    iconBox.onclick = () => {
+        iconDetailBackground.classList.toggle('icon__detail__background__active');
+    }
+});
+document.getElementById('colorPicker').addEventListener('input', function(event) {
+    var selectedColor = event.target.value;
+    document.getElementById('myCircle').setAttribute('fill', selectedColor);
+});
+
+
+
+{/* <script>
+        const colorPicker = document.getElementById('colorPicker');
+        const downloadBtn = document.getElementById('downloadBtn');
+        const svgContainer = document.getElementById('svgContainer');
+
+        colorPicker.addEventListener('input', updateSvgColor);
+        downloadBtn.addEventListener('click', downloadSvgAsPng);
+
+        function updateSvgColor() {
+            const selectedColor = colorPicker.value;
+            svgContainer.querySelector('rect').setAttribute('fill', selectedColor);
+        }
+
+        function downloadSvgAsPng() {
+            const svgContent = svgContainer.innerHTML;
+            const blob = new Blob([svgContent], { type: 'image/svg+xml' });
+            const url = URL.createObjectURL(blob);
+
+            const canvas = document.createElement('canvas');
+            const context = canvas.getContext('2d');
+            const image = new Image();
+
+            image.onload = function () {
+                canvas.width = image.width;
+                canvas.height = image.height;
+                context.drawImage(image, 0, 0);
+
+                const pngUrl = canvas.toDataURL('image/png');
+                const a = document.createElement('a');
+                a.href = pngUrl;
+                a.download = 'colored_svg.png';
+                document.body.appendChild(a);
+                a.click();
+                document.body.removeChild(a);
+                URL.revokeObjectURL(url);
+            };
+
+            image.src = url;
+        }
+    </script> */}
